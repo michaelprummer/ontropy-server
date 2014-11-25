07b1d3736b760a684f8d48a3c2e5a3895f3a6222
BenSower <sauer.benjamin@gmail.com>
2014-11-25 15:01:21 +0100

    added post - create method

--- ontropy-server/routes/api.js
+++ ontropy-server/routes/api.js
@@ -20,13 +20,36 @@ router.get('/read/:lvlId', function(req, res) {
 	//TODO: SANITIZE INPUT! 
 	var lvlQuery = (req.params.lvlId != undefined) ? { "_id" : req.params.lvlId } : {} ;
 
-	Level.find(lvlQuery, function (err, docs) {
+	Level.findOne(lvlQuery, function (err, docs) {
 		if (err) throw err;
         res.contentType('application/json');
         res.write(JSON.stringify(docs));
         res.end();
 	});
 });
+
+//save new level
+router.post('/create', function(req, res) {
+
+	//TODO: SANITIZE INPUT! 
+	var newLvl = req.body;
+	console.log(newLvl);
+
+	Level.create({
+			name : newLvl.name ,
+			content: newLvl.content
+		},
+		function(err, elem) {
+			if (err) throw err;
+			//console.log("Saved level " + elem);
+			res.contentType('application/json');
+        	res.write(JSON.stringify(elem));
+        	res.end();
+	});
+});
+
+
+
 
 
 module.exports = router;
