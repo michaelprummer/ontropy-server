c87eb1ca0a800d7de46af91aedfc92065e3f1c9a
BenSower <sauer.benjamin@gmail.com>
2014-11-25 14:40:51 +0100

    get single level by id

--- ontropy-server/routes/api.js
+++ ontropy-server/routes/api.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var Level = require('../app/model/level').Level;
 
-/* GET home page. */
+/* GET all levels */
 router.get('/read', function(req, res) {
-
-
-	var Level = require('../app/model/level').Level;
 
 	Level.find({}, function (err, docs) {
 		if (err) throw err;
@@ -16,6 +14,19 @@ router.get('/read', function(req, res) {
 	});
 });
 
+//get single level by id
+router.get('/read/:lvlId', function(req, res) {
+
+	//TODO: SANITIZE INPUT! 
+	var lvlQuery = (req.params.lvlId != undefined) ? { "_id" : req.params.lvlId } : {} ;
+
+	Level.find(lvlQuery, function (err, docs) {
+		if (err) throw err;
+        res.contentType('application/json');
+        res.write(JSON.stringify(docs));
+        res.end();
+	});
+});
 
 
 module.exports = router;
